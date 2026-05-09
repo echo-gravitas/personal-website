a4dbfeacc0095aeefd1d43b30cd3f38a8386dc1a
Ralph Bolliger <ralph.bolliger@pm.me>
2026-05-09 18:06:34 +0200

    Added some more themes

--- src/scripts/themes.ts
+++ src/scripts/themes.ts
@@ -1,24 +1,38 @@
 export const themeTokens = {
   dark: {
-    '--theme-page': '#060606',
-    '--theme-font': '#f8f8f5',
-    '--theme-muted': '#a1a1a1',
-    '--theme-accent': '#f8f8f5',
-    '--theme-border': '#232323',
+    page: '#060606',
+    font: '#f8f8f5',
+    muted: '#a1a1a1',
+    accent: '#f8f8f5',
+    border: '#232323',
   },
   light: {
-    '--theme-page': '#f8f8f5',
-    '--theme-font': '#060606',
-    '--theme-muted': '#737373',
-    '--theme-accent': '#060606',
-    '--theme-border': '#d9d9d9',
+    page: '#f8f8f5',
+    font: '#060606',
+    muted: '#737373',
+    accent: '#060606',
+    border: '#d9d9d9',
   },
   blue: {
-    '--theme-page': '#124259',
-    '--theme-font': '#dcd4cb',
-    '--theme-muted': '#737373',
-    '--theme-accent': '#dcd4cb',
-    '--theme-border': '#dcd4cb',
+    page: '#124259',
+    font: '#dcd4cb',
+    muted: '#949494',
+    accent: '#dcd4cb',
+    border: '#5D7781',
+  },
+  brown: {
+    page: '#2B2827',
+    font: '#d9b478',
+    muted: '#76654F',
+    accent: '#d9b478',
+    border: '#5B4E3D',
+  },
+  velvet: {
+    page: '#2a1f3e',
+    font: '#e2bfa4',
+    muted: '#6D5A62',
+    accent: '#e2bfa4',
+    border: '#332945',
   },
 } as const;
 
@@ -38,7 +52,7 @@ export const applyTheme = (
 ) => {
   element.dataset.theme = theme;
 
-  Object.entries(themeTokens[theme]).forEach(([property, value]) => {
-    element.style.setProperty(property, value);
+  Object.entries(themeTokens[theme]).forEach(([token, value]) => {
+    element.style.setProperty(`--theme-${token}`, value);
   });
 };
