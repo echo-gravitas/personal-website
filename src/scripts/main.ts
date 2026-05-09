0a18e4dbc862a47bb0ba82224693333d4a4a5e58
Ralph Bolliger <ralph.bolliger@pm.me>
2026-05-09 12:53:57 +0200

    Created theme switcher and added two themes.

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,1 +1,25 @@
-console.log('Going dark is no longer an option.');
+const themes = ['dark', 'light'] as const;
+type Theme = (typeof themes)[number];
+
+const getTheme = (): Theme => {
+  const theme = document.documentElement.dataset.theme;
+
+  return theme === 'light' || theme === 'dark' ? theme : 'dark';
+};
+
+const setTheme = (theme: Theme) => {
+  document.documentElement.dataset.theme = theme;
+
+  try {
+    localStorage.setItem('theme', theme);
+  } catch {
+  }
+};
+
+document.querySelector('[data-theme-toggle]')?.addEventListener('click', () => {
+  const currentTheme = getTheme();
+  const currentIndex = themes.indexOf(currentTheme);
+  const nextTheme = themes[(currentIndex + 1) % themes.length];
+
+  setTheme(nextTheme);
+});
