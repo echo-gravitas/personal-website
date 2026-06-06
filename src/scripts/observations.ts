95e100236130a19978743a61a460c743a3ab1ced
Ralph Bolliger <ralph.bolliger@pm.me>
2026-06-06 15:16:15 +0200

    WIP Blog

---
+++ src/scripts/observations.ts
@@ -0,0 +1,6 @@
+export const observationTopicLabels = {
+  'cyber-security': 'Cyber Security',
+  'datenschutz': 'Datenschutz',
+  'ki': 'KI',
+  'security-research': 'Security Research',
+} as const;
